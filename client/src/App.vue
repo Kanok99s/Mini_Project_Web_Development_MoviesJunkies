ad8809dbccb9adebbd08902fded040f8617901d1
nazlim <nazlim@student.chalmers.se>
2022-09-26 10:23:15 +0200

    adding the frontend and the backend of sign-in

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,11 +2,22 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link>
+      <router-link to="/Signin">Signin</router-link>
     </div>
     <!-- Render the content of the current page view -->
     <router-view/>
   </div>
 </template>
+<script>
+import { Signin } from '@/components/Signin.vue'
+
+export default {
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    signin: Signin
+  }
+}
+</script>
 
 <style>
 #app {
--- client/src/main.js
+++ client/src/main.js
@@ -2,15 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
+import Signin from './views/Signin.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
+const routes = [
+  { path: '/users', component: Signin }]
+
+const route = new VueRouter({
+  routes
+})
 
 new Vue({
   router,
+  route,
   render: function (h) { return h(App) }
 }).$mount('#app')
